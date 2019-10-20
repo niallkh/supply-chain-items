86508be5937656e306bf30d987d8b0b6a604994f
Nail Khafizov <854297992c@gmail.com>
2019-10-20 18:31:39 +0300

    deploy to rinkeby

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,3 +1,11 @@
+const HDWallet = require('truffle-hdwallet-provider');
+const keys = require('./env.json');
+const infuraKey = keys.apiKey;
+const mnemonic = keys.wallet;
+
+// address: 0x5C3554E31DA34091ECcC85f2C616b8371BC687AD
+// tx: 0xc7130313f9c946d8185af0cf08b3717f4446fab0f782190686be17ea75e06bb5
+
 module.exports = {
   networks: {
     development: {
@@ -10,6 +18,13 @@ module.exports = {
       network_id: 20,
       accounts: 10,
       defaultEtherBalance: 100000
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWallet(
+        mnemonic,
+        `https://rinkeby.infura.io/v3/${infuraKey}`
+      ),
+      network_id: 4,
+    },
   }
 };
