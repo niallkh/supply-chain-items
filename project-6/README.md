5822e9d2120e1dfb5fa6e1fc6ad5e9ef84a1594a
Nail Khafizov <854297992c@gmail.com>
2019-10-20 23:46:39 +0300

    added readme

---
+++ project-6/README.md
@@ -0,0 +1,13 @@
+address (rinkeby) = 0x5C3554E31DA34091ECcC85f2C616b8371BC687AD
+
+tx (rinkeby) = 0xc7130313f9c946d8185af0cf08b3717f4446fab0f782190686be17ea75e06bb5
+
+Program version numbers: 1
+
+Truffle v5.0.37 (core: 5.0.37)
+
+Solidity v0.5.8 (solc-js)
+
+Node v10.16.3
+
+Web3.js v1.2.1
--- project-6/truffle.js
+++ project-6/truffle.js
@@ -3,9 +3,6 @@ const keys = require('./env.json');
 const infuraKey = keys.apiKey;
 const mnemonic = keys.wallet;
 
-// address: 0x5C3554E31DA34091ECcC85f2C616b8371BC687AD
-// tx: 0xc7130313f9c946d8185af0cf08b3717f4446fab0f782190686be17ea75e06bb5
-
 module.exports = {
   networks: {
     development: {
